c2b30705f0ca398cc880fa513b44e0f3112e4c57
Utkarsh Goel <utkarsh867@users.noreply.github.com>
2020-07-21 17:00:32 +0530

    Fixes theme font destructure for chakra

--- src/gatsby-plugin-chakra-ui/theme.js
+++ src/gatsby-plugin-chakra-ui/theme.js
@@ -8,7 +8,7 @@ const customTheme = {
     brandBlue: "#0099AA",
   },
   fonts: {
-    ...theme,
+    ...theme.fonts,
     body: "Poppins, system-ui, sans-serif",
     heading: "Poppins, system-ui, sans-serif",
     mono: "Menlo, monospace",
