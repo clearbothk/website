0d05b5ef3c6fbfe5c57dacada668a57f84f814d3
--replace-all <utkarsh867@gmail.com>
2021-03-02 19:11:00 +0800

    Add URL for review production env

--- src/constants/azureAPI.ts
+++ src/constants/azureAPI.ts
@@ -6,8 +6,8 @@ export const uploadTokenAPIURI =
 export const reviewFetchURI =
   process.env.NODE_ENV === "development"
     ? "/api/GetReviewImage"
-    : "https://clearbotdatabuilder0.azurewebsites.net/api/GetReviewImage?code=jV6T1zWPoGYaZFSum2Wf2uN1rRLvFk5meRuRAAHbR53FfRsUTZIPNw=="
+    : "https://clearbotdatabuilder0.azurewebsites.net/api/GetReviewImage?code=UrMAqhiGSuRSxPtDQZA6Iv32u0P0NF5zOCirj/iqeY9W9Wtyx8PVdg=="
 export const reviewCompleteURI =
   process.env.NODE_ENV === "development"
     ? "/api/CompleteReviewImage"
-    : "https://clearbotdatabuilder0.azurewebsites.net/api/CompleteReviewImage?code=jV6T1zWPoGYaZFSum2Wf2uN1rRLvFk5meRuRAAHbR53FfRsUTZIPNw=="
+    : "https://clearbotdatabuilder0.azurewebsites.net/api/CompleteReviewImage?code=yBRH4dk1dYJkddn9fp4Fci9rBEdeu4wRwiq3fZZPuRA0dXxWZGmJqQ=="
